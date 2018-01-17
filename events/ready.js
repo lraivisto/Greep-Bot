70f80d23c48d659291999a6cd2d5270397be513f
GreepingGuy <34273712+GreepingGuy@users.noreply.github.com>
2018-01-17 18:00:16 +0200

    ready.js code cleanup

--- events/ready.js
+++ events/ready.js
@@ -5,15 +5,17 @@ const chalk = require('chalk');
 const moment = require('moment');
 
 module.exports = client => { // eslint-disable-line no-unused-vars
+
   client.user.setPresence({
+  
     status: settings.status,
     game: {
       name: settings.game,
       type: 2
     }
   });
-  console.log(`----------`)
+  console.log(``)
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Loaded every module successfully.`)
  
-  console.log(`I am ready for ${client.guilds.size} servers, and ${client.users.size} users.`);
+  console.log(`Online, ${client.guilds.size} servers, ${client.users.size} users.`);
  }
