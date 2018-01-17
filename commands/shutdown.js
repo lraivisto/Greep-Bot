5d768f0268921915f424d38c8c178d1eda3080ab
GreepingGuy <34273712+GreepingGuy@users.noreply.github.com>
2018-01-17 19:12:25 +0200

    Code clean
    shutdown command code clean

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -1,29 +1,31 @@
+const Discord = require('discord.js');
 exports.run = async (client, message, args, level) => {// eslint-disable-line no-unused-vars
- const Discord = require('discord.js');
 
         if(message.author.id!='396003871434211339'){
                 message.channel.send("Only the founder can execute this.") }
         else { 
   
-  var e = new Discord.RichEmbed()
+          var e = new Discord.RichEmbed()
   
-   
-    .setTitle(`Greep - Bot Shutdown`)
-    .setDescription("The bot is stopping every module safely and is now shutting down temporarily.")
-    .setColor('#ECEDEE')
-    .setFooter(`Success | ${message.createdAt}`)
-    .setThumbnail(`https://cdn.discordapp.com/attachments/398185503045976064/402893358655340554/Shutdown_PNG_White.png`)
-   
-   await message.channel.send("",{embed:e});
+          .setTitle(`Greep - Bot Shutdown`)
+          .setDescription("The bot is stopping every module safely and is now shutting down temporarily.")
+          .setColor('#ECEDEE')
+          .setFooter(`Success | ${message.createdAt}`)
+          .setThumbnail(`https://cdn.discordapp.com/attachments/398185503045976064/402893358655340554/Shutdown_PNG_White.png`)
+  
+          await message.channel.send("",{embed:e});
     
-    message.delete();
-  
+          message.delete();
 
-  client.commands.forEach( async cmd => {
-    await client.unloadCommand(cmd);
-  });
-  process.exit(1);
-};
+          client.commands.forEach( async cmd => {
+            await client.unloadCommand(cmd);
+  
+          });
+  
+          process.exit(1);
+
+        };
+
 }
 
 exports.conf = {
