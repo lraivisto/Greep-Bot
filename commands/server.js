404fb1f1341cb78271b468ce4aaf50b1594e0014
GreepingGuy <34273712+GreepingGuy@users.noreply.github.com>
2018-01-17 20:21:26 +0200

    Added  server memory and OS to stats
    Added system memory usage and OS to statistics

--- commands/server.js
+++ commands/server.js
@@ -1,35 +1,41 @@
+const { version } = require("discord.js");
+const os = require('os');
 const Discord = require('discord.js');
-exports.run = (client, message) => {
+const moment = require("moment");
+require("moment-duration-format");
+exports.run = (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]"); 
 
-    var e = new Discord.RichEmbed()
-   
-    .setAuthor("Greep - Guild Stats",message.client.avatarURL)
-    .setDescription(`Here are your requested guild stats, ${message.author.username}`)
-    .addField("Guild name:",message.guild.name)
-    .addField("Guild region:",message.guild.region)
-    .addField("Guild owner:",message.guild.owner.user.username)
-    .addField("Guild icon (URL):",message.guild.iconURL)
-    .addField("Guild ID:",message.guild.id)
-    .addField("Guild members:",message.guild.memberCount)
-    .addField("Guild role count:",message.guild.roles.filter(roles => roles).size)
-    .addField("Guild channel count:",message.guild.channels.filter(channels => channels).size)
-    .addField("Guild created at:",message.guild.createdAt)
-    .setColor('#ECEDEE')
-    .setThumbnail(`https://cdn.discordapp.com/attachments/398185503045976064/402891300195794944/server_white.png`)
-   
-    message.channel.send("",{embed:e});
+  var e = new Discord.RichEmbed()
+
     
+    .setTitle(`Greep - Statistics`)
+    .setDescription("Here is the bot's current statistics.")
+    .addField("Bot Uptime",`\`${duration}\``)
+    .addField("Users",`\`${client.users.size.toLocaleString()}\``)
+    .addField("Channels",`\`${client.channels.size.toLocaleString()}\``)
+    .addField("Servers",`\`${client.guilds.size.toLocaleString()}\``)
+    .addField("Library",`\`discord.js v${version}\``)
+    .addField("Node",`\`${process.version}\``)
+    .addField("Memory Usage",`\`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.floor(os.totalmem() / 1024**3 * 100) / 100}GB\``)
+    .addField("Server OS",`\`${os.type()}\``)
+    .setColor('#ECEDEE')
+    .setThumbnail(`https://cdn.discordapp.com/attachments/398185503045976064/402893841688035329/Statistics_White_PNG.png`)
+    .setFooter("Requested by: "+message.author.username, message.author.avatarURL)
+
+    message.channel.send("",{embed:e});
 }
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [],
-  permLevel: 0
+  aliases: ['stat', 'bstats', 'bstat'],
+  permLevel: "User"
 };
 
 exports.help = {
-  name: 'guild',
-  description: 'View the stats of the server.',
-  usage: 'guild'
-}
+  name: "stats",
+  category: "Miscelaneous",
+  description: "Gives some useful bot statistics",
+  usage: "stats"
+};
