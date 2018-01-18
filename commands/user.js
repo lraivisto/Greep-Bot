1e08b58902235b272dbdfe5761129a3eec6ea8e4
GreepingGuy <34273712+GreepingGuy@users.noreply.github.com>
2018-01-18 20:32:33 +0200

    user.js - Embed Fixes
    More embed fixes...

--- commands/user.js
+++ commands/user.js
@@ -4,14 +4,14 @@ exports.run = (client, message) => {
     var mentions = message.mentions.users.filter(user=>{if(user !== client.user) return user;}).first();
     var member = message.guild.member(mentions)||message.member;
     var e = new Discord.RichEmbed()
-    .setAuthor("Greep - User Stats",message.client.avatarURL)
+    .setAuthor("Greep - User Stats",client.user.avatarURL)
     .setDescription("Here are your requested user statistics.")
     .addField("Username:",member.user.username)
     .addField("Discriminator:",member.user.discriminator)
     .addField("Client ID",member.user.id)
     .addField("User created at:",member.user.createdAt)
     .addField("Is the specified user a bot?",member.user.bot)
-    .addField("User Avatar (url)",member.user.avatarURL)
+    .addField("User Avatar in URL",member.user.avatarURL)
     .setFooter("Requested by: "+message.author.username, message.author.avatarURL)
     .setColor('#ECEDEE')
     .setThumbnail(`https://cdn.discordapp.com/attachments/398185503045976064/402889511715078144/White_User_PNG.png`)
