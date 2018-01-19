d82c5076e38f980a7f64c41d069c5f3ecd1e264c
GreepingGuy <34273712+GreepingGuy@users.noreply.github.com>
2018-01-19 16:03:33 +0200

    Purge - Embed and alias added
    Alias added: g"clear

--- commands/purge.js
+++ commands/purge.js
@@ -1,9 +1,22 @@
+const Discord = require('discord.js');
+const embed = new Discord.RichEmbed();
 exports.run = (client, message, args) => {
 const messagecount = parseInt(args.join(' '));
  
   message.channel.fetchMessages({
     limit: messagecount
   }).then(messages => message.channel.bulkDelete(messages));
+ 
+   var e = new Discord.RichEmbed()
+   
+    .setAuthor("Greep - Purge",client.user.avatarURL)
+    .addField("Messages Purged:",args[0])
+    .setThumbnail('https://cdn.discordapp.com/attachments/398185503045976064/403910716572041216/Delete_White_PNG.png')
+    .setColor('#ECEDEE')
+    .setFooter(`Success | ${message.createdAt}`)
+
+   
+    message.channel.send("",{embed:e});
 };
 
 exports.conf = {
